0bc294f872f0fe0302e8af12817a422fe49e3d4a
selina-islam <digitalselina@gmail.com>
2024-06-01 13:33:08 +0600

    assingment two of JavaScript

---
+++ assign.js
@@ -0,0 +1,170 @@
+// numner 1
+
+/*function celsiusToFahrenheit(celsius) {
+        return (celsius * (9/5))  +32 ;
+}
+ console.log(celsiusToFahrenheit(0));
+ console.log(celsiusToFahrenheit(25));*/  
+ 
+
+
+
+ //number 2
+
+/* function isEven(num){
+        return num % 2 === 0;
+ }
+console.log(isEven(4)); 
+console.log(isEven(7)); */
+
+
+
+
+//number 3
+
+/*function sum(a, b){
+        return a + b
+}
+console.log(sum(3, 4)); 
+console.log(sum(10, 20)); */
+
+
+
+
+
+//number 4
+
+/*function findSmallestNum(arr) {
+        return Math.min (...arr);
+}
+
+console.log(findSmallestNum([3, 5, 1, 9])); 
+console.log(findSmallestNum([-1, -5, 0, 10])); */
+
+
+
+
+//number 5
+
+/*function countVowels(str) {
+        let vowels = 'aeiouAEIOU';
+     return Array.from(str).filter((char)=> vowels.includes(char)); 
+}
+console.log(countVowels("hello world")); 
+console.log(countVowels("Javascript")); */
+
+
+
+
+
+//number 6
+
+/*function getFirstElement(arr){
+        return arr[0];
+}
+console.log(getFirstElement([1, 2, 3])); 
+console.log(getFirstElement(["a", "b", "c"])); */
+
+
+
+
+//number 7
+
+/*function isArrayEmpty(arr){
+        return arr.length === 0;
+}
+console.log (isArrayEmpty([]))
+console.log(isArrayEmpty([1, 2, 3]))*/
+
+
+
+
+//number 8
+
+/*function factorialize(num) {
+        if (num === 0 || num === 1) return 1;
+        return factorialize(num - 1) * num;
+}
+console.log(factorialize(5));
+console.log(factorialize(7));*/
+
+
+
+
+//number 9
+
+/*function reverseString(str) {
+     return str.split('').reverse().join('');  
+}
+console.log(reverseString("hello"));
+console.log(reverseString("world"));*/
+
+
+
+
+//number 10
+
+/*function toLowerCase(str){
+        return str.toLowerCase();
+}
+console.log(toLowerCase("HELLO WORLD"));
+console.log(toLowerCase("JavaScrpt"));*/
+
+
+
+
+
+//number 11
+
+/*function stringLength(str){
+        return str.length;
+}
+console.log(stringLength("hello"));
+console.log(stringLength('world'));*/
+
+
+
+
+
+//number 12
+
+/*function mergeArrays(arr1, arr2){
+        return arr1.concat(arr2);
+}
+console.log(mergeArrays([1, 2, 3], [4, 5, 6]));
+console.log(mergeArrays(["a", "b"], ["c", "d"]));*/
+
+
+
+
+//number 13
+
+/*function getLastElement(arr){
+        return arr[arr.length - 1];
+}
+console.log(getLastElement([1, 2, 3])); 
+console.log(getLastElement(["a", "b", "c"]));*/
+
+
+
+
+
+//number 14
+
+/*function getFirstCharacter(str) {
+        return str.charAt(0);
+}
+console.log(getFirstCharacter("hello"));
+console.log(getFirstCharacter("world"));*/
+
+
+
+
+//number 15
+
+/*function sumArray(arr){
+        return arr.reduce((a,b) => a+b );
+}
+console.log(sumArray([1, 2, 3, 4])); 
+console.log(sumArray([-1, -2, -3, -4])); 
+console.log(sumArray([1.5, 2.5, 3.5])); */
